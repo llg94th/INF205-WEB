81f9564fbbd1f7227f67621288c078b63be5c9aa
phimhph04071 <santafe9xyb@gmail.com>
2016-12-14 14:42:40 +0000

    Edit addproduct

--- add-product.php
+++ add-product.php
@@ -3,16 +3,16 @@
 	$db = new DB_Connect();
 	$conn = $db->connect();
 	$respon = array();
-	if(isset($_REQUEST['name'])&&isset($_REQUEST['price'])){
-		$sqlgetid ="SELECT `id` FROM `customers` ORDER BY `id` DESC LIMIT 1";
+	if(isset($_REQUEST['name'])&&isset($_REQUEST['price'])&&isset($_REQUEST['category'])){
+		$sqlgetid ="SELECT `id` FROM `product` ORDER BY `id` DESC LIMIT 1";
 		$result =$conn->query($sqlgetid);
 		if ($row =mysqli_fetch_assoc($result)){
 			$id=$row['id'];
 		}
 		$sao=substr($id,2);
 		$sao2=(int) $sao;
-		$nextid = "SP".(((int) substr($id,2))+1);
-		$sql = "INSERT INTO `product` (`id`, `product_name`, `price`) VALUES ('".$nextid."', '".$_REQUEST['name']."', ".$_REQUEST['price'].");";
+		$nextid = "SP0".(((int) substr($id,2))+1);
+		$sql = "INSERT INTO `product` (`id`, `product_name`, `price`, `category_id`) VALUES ('".$nextid."', '".$_REQUEST['name']."', ".$_REQUEST['price'].",".$_REQUEST['category'].");";
 		if($conn->query($sql)){
 			$respon['success']=1;	
 		}  else {
--- update-product.php
+++ update-product.php
@@ -3,8 +3,8 @@
 	$db = new DB_Connect();
 	$conn = $db->connect();
 	$respon = array();
-	if(isset($_REQUEST['id'])&&isset($_REQUEST['name'])&&isset($_REQUEST['price'])){
-		$sql = "UPDATE `product` SET `product_name`='".$_REQUEST['name']."',`price`=".$_REQUEST['price']." WHERE `id`='".$_REQUEST['id']."'";
+	if(isset($_REQUEST['id'])&&isset($_REQUEST['name'])&&isset($_REQUEST['price'])&&isset($_REQUEST['category'])){
+		$sql = "UPDATE `product` SET `product_name`='".$_REQUEST['name']."',`price`=".$_REQUEST['price'].",`category_id` =".$_REQUEST['category']." WHERE `id`='".$_REQUEST['id']."'";
 		if($conn->query($sql)){
 			$respon['success']=1;	
 		}  else {
